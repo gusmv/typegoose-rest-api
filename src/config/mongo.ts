f1f785c308512b1c13fba7ab46d63a3d5d006ace
Gustavo Vieira <gustavo.vieira@enacom.com.br>
2021-03-07 11:44:08 -0300

    Add Typegoose to project

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,3 +1,4 @@
 export default {
+  debug: true,
   uri: process.env.URI || "mongodb://localhost/rest-api",
 };
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 import config from "@config/mongo";
 
+mongoose.set("debug", config.debug);
+
 mongoose.connect(config.uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
